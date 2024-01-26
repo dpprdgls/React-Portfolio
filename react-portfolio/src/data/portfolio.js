9fe7e4fd9de2b0177cbe1957f85eb304d392389b
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-01-25 23:49:05 -0800

    add images

--- react-portfolio/src/data/portfolio.js
+++ react-portfolio/src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'MVC Tech Blog',
-        imgUrl: '',
+        imgUrl: '../assets/mvctechblog.png',
         stack: ['JavaScript', 'Handlebars', 'CSS'],
         link: 'https://the-mvc-tech-blog-75a3ecb0b535.herokuapp.com/'
     },
     {
         title: 'Geeks Garage',
-        imgUrl: '',
+        imgUrl: '../assets/geeksgarage.png',
         stack: ['JavaScript', 'HandleBars', 'CSS'],
         link: 'https://geeks-garage-cd-bfe5921b83db.herokuapp.com/'
     },
     {
         title: 'Rain or Shine Weather',
-        imgUrl: '',
+        imgUrl: '../assests/weatherdashboard.png',
         stack: ['JavaScript', 'CSS', 'HTML'],
         link: 'https://dpprdgls.github.io/Rain-or-Shine-Weather-Dashboard/'
     },
