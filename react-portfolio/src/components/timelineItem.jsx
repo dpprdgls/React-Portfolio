42a74e52c3d2dc91ff6781b114f0d31b57236cfa
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-01-26 00:24:02 -0800

    add images to public

---
+++ react-portfolio/src/components/timelineItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TimelineItem() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default TimelineItem;
---
+++ react-portfolio/src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        year: 'Current',
+        title: 'Automation and Controls Technician',
+        duration: '1 Year',
+        details: 'My main focus has been the implementation of optimization to the automation code and the manufacturing processes associated with it.'
+    },
+    {
+        year: '2020-2023',
+        title: 'Laser Welding Technician',
+        duration: '3 Years',
+        details: 'The main objective was to improve the laser welding first pass yield and the maintenance of the equipment to reduce downtime.'
+    },
+    {
+        year: 'Currently',
+        title: 'Engineering Technician',
+        duration: '2018-2020',
+        details: 'During the Model 3 launch it was my reponsibility to work with the engineering team to develop and improve the automated shipping system to sustain lean manufacturing process.'
+    }
+]
