38a36e50bc36fd742bf6fbfae26c02a563faff52
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-01-25 23:40:03 -0800

    add projects

--- react-portfolio/src/components/portfolioItem.jsx
+++ react-portfolio/src/components/portfolioItem.jsx
@@ -10,9 +10,9 @@ function PortfolioItem({ title, imgUrl, stack, link}) {
             />
             <div className='w-full p-4'>
                 <h3 classname='text-lg md:text-xl mb-2 md:m-3 font-semibold'>{title}</h3>
-                <p>
+                <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm'>
                     {stack.map(item => (
-                        <span>{item}</span>
+                        <span className='inline-block px-2 py-1 font semibold border-2 border-stone 900 rounded-md'>{item}</span>
                     ))}
                 </p>
             </div>
---
+++ react-portfolio/src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'MVC Tech Blog',
+        imgUrl: '',
+        stack: ['JavaScript', 'Handlebars', 'CSS'],
+        link: 'https://the-mvc-tech-blog-75a3ecb0b535.herokuapp.com/'
+    },
+    {
+        title: 'Geeks Garage',
+        imgUrl: '',
+        stack: ['JavaScript', 'HandleBars', 'CSS'],
+        link: 'https://geeks-garage-cd-bfe5921b83db.herokuapp.com/'
+    },
+    {
+        title: 'Rain or Shine Weather',
+        imgUrl: '',
+        stack: ['JavaScript', 'CSS', 'HTML'],
+        link: 'https://dpprdgls.github.io/Rain-or-Shine-Weather-Dashboard/'
+    },
+]
