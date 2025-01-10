42cb3696b00d5204c64f8d15d407197da0a7424e
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2025-01-09 23:37:42 -0800

    add spacing

--- react-portfolio/src/components/portfolioItem.jsx
+++ react-portfolio/src/components/portfolioItem.jsx
@@ -13,7 +13,7 @@ function PortfolioItem({ title, imgUrl, stack, link, github }) {
         <h3 className='text-lg md:text-xl mb-2 md:m-3 font-semibold'>{title}</h3>
         <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm'>
           {stack.map((item, index) => (
-            <span key={index} className='inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md'>
+            <span key={index} className='inline-block px-2 py-2 font-semibold border-2 border-stone-900 rounded-md'>
               {item}
             </span>
           ))}
